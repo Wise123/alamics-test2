c87a122663772bc3607c65f1ed4971b4931c04f5
Wise <roman_nagaev_1993@mail.ru>
2019-02-23 20:01:47 +0500

    подключил swagger

--- src/main/java/org/alamics/controller/TestController.java
+++ src/main/java/org/alamics/controller/TestController.java
@@ -1,14 +1,18 @@
 package org.alamics.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@Api(value="test", description="Тестовый контроллер")
 public class TestController {
 
     @GetMapping("/hello")
+    @ApiOperation(value = "Привет")
     public String hello(){
         return "hello world";
     }
