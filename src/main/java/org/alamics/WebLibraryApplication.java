2d56eb5be261f1a426c2754f59a46ae090b97297
Wise <roman_nagaev_1993@mail.ru>
2019-02-23 19:43:48 +0500

    поднял spring boot

---
+++ src/main/java/org/alamics/WebLibraryApplication.java
@@ -0,0 +1,11 @@
+package org.alamics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebLibraryApplication.class, args);
+    }
+}
